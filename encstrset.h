52bdfdedffba8a99dadca1002e3b564e58287ed0
Hipokid <73963567+Hipokid@users.noreply.github.com>
2020-11-05 15:11:25 +0100

    Wersja 0.1
    Coś tu nie działa :(

---
+++ encstrset.h
@@ -0,0 +1,39 @@
+#ifndef ENCSTRSET_H
+#define ENCSTRSET_H
+
+#ifdef __cplusplus
+	#include <iostream>
+	#include <unordered_map>
+	#include <unordered_set>
+	#include <climits>
+	extern "C"
+	{
+		namespace jnp1
+		{
+#else
+	#include <stdbool.h>
+	#include <stdlib.h>
+#endif
+
+unsigned long encstrset_new();
+
+void encstrset_delete(unsigned long id);
+
+size_t encstrset_size(unsigned long id);
+
+bool encstrset_insert(unsigned long id, const char* value, const char* key);
+
+bool encstrset_remove(unsigned long id, const char* value, const char* key);
+
+bool encstrset_test(unsigned long id, const char* value, const char* key);
+
+void encstrset_clear(unsigned long id);
+
+void encstrset_copy(unsigned long src_id, unsigned long dst_id);
+
+#ifdef __cplusplus
+		}
+	}
+#endif
+
+#endif
